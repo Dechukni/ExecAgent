25013b28c74a7f7558ed54b42fde193a18766450
dechukni <dimazez@ukr.net>
2016-08-26 15:54:38 +0300

    Initial prototype commit

---
+++ src/index.js
@@ -0,0 +1,61 @@
+'use strict';
+
+import * as ws from 'websocket';
+
+export default class WebsocketTest {
+  constructor() {
+    this._name = 'WebSocket';
+    this.socket = new ws.w3cwebsocket('ws://localhost:9000/connect');
+    this.handlers = [];
+  }
+  returnName() {
+    return this._name;
+  }
+  onOpenPromise() {
+    return new Promise(
+      (resolve) => {
+        this.socket.onopen = () => {
+          this._name = 'OpenedWebSocket';
+          resolve(this._name);
+        };
+      }
+    );
+  }
+  sendTestMessage(command, id, body) {
+    this.socket.send(
+      JSON.stringify({
+        operation: command,
+        id: String(id),
+        body: body
+      })
+    );
+  }
+  onMessagePromise() {
+    return new Promise(
+      (resolve, reject) => {
+        this.socket.onmessage = (event) => {
+          let parsedEvent = JSON.parse(event.data);
+
+          for (let handler of this.handlers) {
+            handler(parsedEvent);
+          }
+          if (!('error' in parsedEvent) || (parsedEvent.error === null)) {
+            resolve(parsedEvent);
+          } else {
+            reject(parsedEvent.error);
+          }
+        };
+      }
+    );
+  }
+  addEventHandler(handler) {
+    this.handlers.push(handler);
+  }
+  removeEventHandler(handler) {
+    let handlerIndex = this.handlers.indexOf(handler);
+
+    if (handlerIndex !== -1) {
+      this.handlers.splice(handlerIndex, 1);
+    }
+  }
+}
